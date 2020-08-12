224b08b01c9b82e67ff38894deb9a0e68c8c3276
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-08-12 02:05:54 +0200

    Create KEYPAD_Interface.h

---
+++ KEYPAD/KEYPAD_Interface.h
@@ -0,0 +1,18 @@
+/*************************************************************/
+/*  AUTHOR     : AbdElrahman Ibrahim Zaki                    */
+/*  DATE       :  											 */
+/*  VERSION	   : V0.1										 */
+/*  DESCRIPTION: Keypad Interface                              */
+/*************************************************************/
+
+#ifndef Keypad_Interface_H
+#define Keypad_Interface_H
+
+u8 keypad_u8GetPressedKey();
+
+#define KEYPAD_PORT  PORTB
+
+
+
+
+#endif
