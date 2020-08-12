fceefff2d8382f88f45f5b343832af16f98ddbee
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-08-12 02:03:57 +0200

    Create LCD_Interface.h

---
+++ LCD/LCD_Interface.h
@@ -0,0 +1,28 @@
+/****************************************************************/
+/*   Author             :    AbdElrahman Ibrahim Zaki		    */
+/*	 Date 				:    23rd of May 2020 					*/
+/*	 Version 			:    1.0V 							 	*/
+/*	 Description 		:    LCD Interface                       */
+/****************************************************************/
+
+#ifndef LCD_INT_H
+#define LCD_INT_H
+
+void LCD_Ini(void); //
+void LCD_writeChar(u8 DATA);
+void LCD_writeCMD(u8 CMD);
+void Cursor_Pos(u8 x_pos , u8 y_pos);
+void LCD_writeString (char *str);
+u16 LCD_u8writeINT(u16 Copy_u16NB);
+u16 Private_u16Power(u8 Copy_u8Op1, u8 Copy_u8Op2);
+
+#define CONTROL_PORT    PORTC
+#define RS              PIN7
+#define RW              PIN6
+#define EN              PIN5
+#define DATA_PORT       PORTD
+
+
+
+
+#endif
