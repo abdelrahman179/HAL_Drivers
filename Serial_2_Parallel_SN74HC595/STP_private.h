00f503569e39549a5ba332318258dde1f0872611
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-16 09:18:48 +0200

    Create STP_private.h

---
+++ Serial_2_Parallel_SN74HC595/STP_private.h
@@ -0,0 +1,16 @@
+/************************************************************************************/
+/* Author : AbdElrahman I.Zaki                                                      */
+/* Date   : 14 September 2020                                                       */
+/* Version: V01                                                                     */
+/* Desc.  : SN74HC595 Serial to Prallel IC Interface with STM32 private functions   */
+/************************************************************************************/
+
+
+#ifndef STP_PRIVATE_H
+#define STP_PRIVATE_H
+
+
+
+
+
+#endif
