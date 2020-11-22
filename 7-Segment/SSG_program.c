48f251ff921460a86168c1f1a4e8dc56c413de2c
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-22 04:04:42 +0200

    Update SSG_program.c

--- 7-Segment/SSG_program.c
+++ 7-Segment/SSG_program.c
@@ -9,22 +9,22 @@
 #include "BIT_MATH.h"
 
 #include "RCC_interface.h"
-#include "DIO_interface.h"
+#include "GPIO_interface.h"
 
 #include "SSG_interface.h"
 #include "SSG_private.h"
 #include "SSG_config.h" 
 
 
-u8 SSD_Pins[7]  = {SSG_A, SSG_B, SSG_C, SSG_D, SSG_E, SSG_F, SSG_G};
+u8 SSG_Pins[7]  = {SSG_A, SSG_B, SSG_C, SSG_D, SSG_E, SSG_F, SSG_G};
 void SSG_voidInit()
 {
 	switch(SSG_PORT)
 	{
-		case GPIOA:	MRCC_voidEnableClock(APB2, 2);	break;
-		case GPIOB: MRCC_voidEnableClock(APB2, 3);	break;
-		case GPIOC: MRCC_voidEnableClock(APB2, 4);	break;
-		default:									break;
+		case GPIOA: MRCC_voidEnableClock(APB2_BUS, 2);	break;
+		case GPIOB: MRCC_voidEnableClock(APB2_BUS, 3);	break;
+		case GPIOC: MRCC_voidEnableClock(APB2_BUS, 4);	break;
+		default:				        break;
 	}
 	
 	for(u8 i = 0 ; i < 7 ; i++)
@@ -35,10 +35,10 @@ void SSG_voidInit()
 
 void SSG_voidDisplayNumber(u8 Copy_u8Number)
 {
-	//Representing the seven segment number in hex decimal
+	/* Representing the seven segment number in hex decimal */
 	u8 SSG_NumRep[10] = {ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE};
 	
-	//seperates the number in hex value to each pin status
+	/* Separates the number in hex value to each pin status */
 	u8 A_Val = (SSG_NumRep[Copy_u8Number] & 0x01);
 	u8 B_Val = (SSG_NumRep[Copy_u8Number] & 0x02) >> 1;
 	u8 C_Val = (SSG_NumRep[Copy_u8Number] & 0x04) >> 2;
@@ -48,12 +48,12 @@ void SSG_voidDisplayNumber(u8 Copy_u8Number)
 	u8 G_Val = (SSG_NumRep[Copy_u8Number] & 0x40) >> 6;
 	
 	u8 SSG_Values[7] = {A_Val, B_Val, C_Val, D_Val, E_Val, F_Val, G_Val};
-	#if SSG_MODE == COM_ANOD
+	if(SSG_MODE == COM_ANOD)
 		for(u8 i = 0; i < 7; i++)
 		{
 			MGPIO_voidSetPinValue(SSG_PORT, SSG_Pins[i], ~(SSG_Values[i]));
 		}
-	#elif SSG_MODE == COM_CATHOD
+	else if(SSG_MODE == COM_CATHOD)
 		for(u8 i = 0 ; i < 7 ; i++)
 		{
 			MGPIO_voidSetPinValue(SSG_PORT, SSG_Pins[i], SSG_Values[i]);
